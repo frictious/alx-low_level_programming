cb7145e0e3d77ea35f80c62463a491fa36f147d6
frictious <gegbaid@gmail.com>
2022-09-10 15:06:26 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,12 +21,11 @@ int main(void)
 {
 
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit;
-       
 	last_digit = n % 10;
 	if ((n % 10) > 5)
 	{
