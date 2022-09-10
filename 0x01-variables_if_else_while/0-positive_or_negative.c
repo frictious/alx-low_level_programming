237a4fb98470cb3d4ec4a8ad0acd5d9a2faf48dc
frictious <gegbaid@gmail.com>
2022-09-10 10:13:12 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,9 @@
 /* betty style doc for function main goes there */
 /* Main entry point Description: the program will check if a number entered */
 int main(void)
-/** 
- * main - Entry point 
- *
+/**
+ * main - Entry point
  * Description: 'the program's description'
- * 
  * Return: Always 0 (Success)
 */
 {
