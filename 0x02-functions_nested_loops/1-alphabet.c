f9a176024481c59595c48cd2879cf7c53b676eae
frictious <gegbaid@gmail.com>
2022-09-13 16:44:39 -0700

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,16 @@
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);
+
 int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/*Print alphabet function declaration*/
+void print_alphabet(void)
 {
 	int num = 'A';
 
@@ -19,7 +28,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
-	return (0);
 }
 
