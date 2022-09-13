03581db9332b2c65edcfa0451e1b914370838307
frictious <gegbaid@gmail.com>
2022-09-13 16:28:46 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar to the screen
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+
+	return (0);
+
+}
+
